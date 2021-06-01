831c77b5fb79ac6231808a127d38ea279a4c0bf5
arsan13 <arsan.anbu97@gmail.com>
2021-06-01 21:26:36 +0530

    search-basic

--- src/routes/RouteApp.js
+++ src/routes/RouteApp.js
@@ -1,11 +1,13 @@
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AdminHome from '../screens/AdminHome';
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AdminHome from "../screens/AdminHome";
+import Search from "../screens/Search";
 
 function RouteApp() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={AdminHome} />
+        <Route exact path="/" component={AdminHome} />
+        <Route path="/search" component={Search} />
       </Switch>
     </Router>
   );
