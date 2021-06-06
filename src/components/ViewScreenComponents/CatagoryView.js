94c4b43f7f99600659348a87f6258bbc70471468
arsan13 <arsan.anbu97@gmail.com>
2021-06-06 15:46:53 +0530

    Created dynamic search bar

--- src/components/ViewScreenComponents/CatagoryView.js
+++ src/components/ViewScreenComponents/CatagoryView.js
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react';
-import { CategoryData } from '../../services/AdminServices';
-import { ColorOne } from '../../styles/color';
+import { useEffect, useState } from "react";
+import { AiFillDelete, AiFillInfoCircle } from "react-icons/ai";
+import { CategoryData } from "../../services/AdminServices";
+import Search from "../Search";
+import { ColorOne } from "../../styles/color";
 import {
   Card,
   CenterAlign,
@@ -9,45 +11,64 @@ import {
   ContainerRow,
   Imageview,
   LeftAlign,
-} from '../../styles/styled';
+} from "../../styles/styled";
 
 function CatagoryView() {
   const [catagoryData, setCatagoryData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     setCatagoryData(CategoryData());
+    setFilteredData(CategoryData());
   }, []);
+
+  const updateFilteredData = (filterData) => {
+    setFilteredData(filterData);
+  };
+
   return (
     <Container>
+      <Search
+        data={catagoryData}
+        searchKeys={["name", ""]}
+        updateFilteredData={updateFilteredData}
+      />
       <ContainerRow full>
-        {catagoryData.map((value, index) => (
-          <ContainerColumn className='col-md-3' height='50%'>
-            <Card deg='40' nohover>
+        {filteredData.map((value, index) => (
+          <ContainerColumn className="col-md-3" height="50%">
+            <Card deg="40" nohover>
               <Imageview
                 src={value.image}
-                width='50%'
-                style={{ marginTop: '2%' }}
-                alt='no image'
+                width="50%"
+                style={{ marginTop: "2%" }}
+                // alternate="no image"
               />
               <CenterAlign style={{ color: ColorOne }}>
                 {value.name}
                 <br />
                 <div
-                  class='input-group mb-2 mr-sm-2'
-                  style={{ maxWidth: '80%', marginLeft: '10%' }}
+                  class="input-group mb-2 mr-sm-2"
+                  style={{
+                    maxWidth: "80%",
+                    marginLeft: "10%",
+                    textAlign: "center",
+                  }}
                 >
                   <button
-                    className='btn btn-danger form-control'
+                    className="btn btn-danger mr-2 form-control"
                     value={value.name}
                   >
-                    delete
+                    <AiFillDelete size="18" />
+                    {"  "}
+                    Delete
                   </button>
                   <button
-                    className='btn btn-info form-control'
-                    name='addImages'
+                    className="btn btn-info form-control"
+                    name="addImages"
                     value={value.name}
                   >
-                    edit
+                    <AiFillInfoCircle size="18" />
+                    {"  "}View
                   </button>
                 </div>
               </CenterAlign>
