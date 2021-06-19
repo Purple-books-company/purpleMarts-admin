61529cb8ad40f6efaf22f3eac5f19c757d07510a
arsan13 <arsan.anbu97@gmail.com>
2021-06-19 14:30:35 +0530

    sub-category view

--- src/screens/ViewScreens.js
+++ src/screens/ViewScreens.js
@@ -1,34 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import Loader from "../components/Loader";
+import CatagoryView from "../components/ViewScreenComponents/CatagoryView";
+import ProductView from "../components/ViewScreenComponents/ProductView";
+import SupplierView from "../components/ViewScreenComponents/SupplierView";
+import SubCategoryView from "../components/ViewScreenComponents/SubCategoryView";
 
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { useLocation } from 'react-router';
-import Loader from '../components/Loader';
-import CatagoryView from '../components/ViewScreenComponents/CatagoryView';
-import ProductView from '../components/ViewScreenComponents/ProductView';
-import SupplierView from '../components/ViewScreenComponents/SupplierView';
-
-import Nav from '../components/Nav';
-
+import Nav from "../components/Nav";
 
 function ViewScreens() {
   const initialState = {
     category: false,
     supplier: false,
     product: false,
+    subCategory: false,
   };
   const show = useLocation();
   const [loader, setLoader] = useState(false);
   const [mountView, setMountView] = useState(initialState);
-  const [editData, setEditData] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     setLoader(true);
     let newState = { ...initialState };
 
     if (show && show.state) {
+      console.log(show.state.show);
       newState[show.state.show] = true;
       if (show.state.value) {
-        setEditData(show.state.value);
+        setData(show.state.value);
+        console.log(show.state.value);
       }
     } else newState["category"] = true;
 
@@ -37,16 +39,18 @@ function ViewScreens() {
   }, []);
   const handleChange = (e) => {
     if (e.target.value !== "") {
-      console.log(e.target.value);
-
       let newState = { ...initialState };
-      console.log(newState);
       newState[e.target.value] = true;
-
-      console.log("state changed");
 
       setMountView(newState);
     }
+  };
+
+  const showSubCategory = (subCategoryName) => {
+    let newState = { ...initialState };
+    setData(subCategoryName);
+    newState["subCategory"] = true;
+    setMountView(newState);
   };
   return (
     <>
@@ -76,9 +80,10 @@ function ViewScreens() {
             </select>
           </RightAlign> */}
 
-          {!mountView.supplier && !mountView.product && (
-            <CatagoryView data={editData}  />
+          {mountView.category && (
+            <CatagoryView showSubCategory={showSubCategory} />
           )}
+          {mountView.subCategory && <SubCategoryView data={data} />}
           {mountView.supplier && <SupplierView />}
           {mountView.product && <ProductView />}
         </>
