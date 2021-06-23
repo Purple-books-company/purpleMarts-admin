046d6986106037f1af8633766fdec3eeb4e4b70b
aravinthraj98 <aravinth799902@gmail.com>
2021-06-23 19:21:45 +0530

    added form in offers,social media and advertisement

--- src/components/offersComponents/SocialMedia.js
+++ src/components/offersComponents/SocialMedia.js
@@ -1,8 +1,45 @@
+import { ColorOne } from '../../styles/color';
+import {
+  ContainerColumn,
+  Input,
+  Formlable,
+  Title,
+  Imageview,
+  ContainerRow,
+  Submitbutton,
+  LeftAlign,
+  RightAlign,
+} from '../../styles/styled';
+
 const SocialMedia = () => {
   return (
-    <div>
-      <h1>Social Media Page</h1>
-    </div>
+    <>
+      <Title>SOCIAL MEDIA </Title>
+      <ContainerRow dynamic>
+        <ContainerColumn className='col-md-5'>
+          <RightAlign>Social MEDIA NAME</RightAlign>
+        </ContainerColumn>
+        <ContainerColumn className='col-md-5'>
+          <Input type='text' placeholder='name' name='name' required />
+        </ContainerColumn>
+        <ContainerColumn className='col-md-5'>
+          <RightAlign>SOCIAL MEDIA LINK</RightAlign>
+        </ContainerColumn>
+        <ContainerColumn className='col-md-5'>
+          <Input type='text' placeholder='name' name='name' required />
+        </ContainerColumn>
+        <ContainerColumn height='20%' className='col-md-5'>
+          <RightAlign>Image</RightAlign>
+
+          {/* {detail.image.length > 10 && (
+            <Imageview src={detail.image}></Imageview>
+          )} */}
+        </ContainerColumn>
+        <ContainerColumn className='col-md-5'>
+          <Input type='text' name='image' placeholder='imageUrl' required />
+        </ContainerColumn>
+      </ContainerRow>
+    </>
   );
 };
 
--- src/styles/styled.js
+++ src/styles/styled.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { ColorOne, ColorTwo } from "./color";
+import styled from 'styled-components';
+import { ColorOne, ColorTwo } from './color';
 
 // Data not found
 export const Msg = styled.h1`
@@ -11,7 +11,7 @@ export const Msg = styled.h1`
 // End
 
 export const Heading = styled.h2.attrs(() => ({
-  className: "text-primary",
+  className: 'text-primary',
 }))`
   font-weight: bold;
 `;
@@ -19,11 +19,9 @@ export const Title = styled.h5`
   text-align: center;
   color: ${ColorOne};
   justify-content: center;
-  width: 30%;
+  width: auto;
 
   margin: 0.5%;
-  border-radius: 10px;
-  margin-left: 35%;
 `;
 let linear1 = (deg) => {
   let temp = `${deg}deg,${ColorTwo} 10%,white 10%`;
@@ -35,11 +33,11 @@ let linear2 = (deg) => {
 };
 
 export const Card = styled.div`
-  width: ${(props) => (props.width ? props.width : "94%")};
-  height: ${(props) => (props.height ? props.height : "94%")};
+  width: ${(props) => (props.width ? props.width : '94%')};
+  height: ${(props) => (props.height ? props.height : '94%')};
   text-align: center;
 
-  margin: ${(props) => (props.margin ? props.margin : "1.5%")};
+  margin: ${(props) => (props.margin ? props.margin : '1.5%')};
   overflow: scroll;
 
   &::-webkit-scrollbar {
@@ -56,13 +54,13 @@ export const Card = styled.div`
     ${(props) => (props.single ? linear1(140) : linear2(props.deg))}
   );
   &:hover {
-    background: ${(props) => (props.nohover ? "" : ColorOne)};
+    background: ${(props) => (props.nohover ? '' : ColorOne)};
     box-shadow: 4px 4px 7px 0px ${ColorOne};
     color: ${ColorTwo};
   }
 `;
 export const Container = styled.div.attrs(() => ({
-  className: "container-fluid",
+  className: 'container-fluid',
 }))`
   background-color: white;
   height: 100%;
@@ -75,30 +73,30 @@ export const Container = styled.div.attrs(() => ({
 //   <div className={className} {...rest} />
 // );
 export const ContainerRow = styled.div.attrs((props) => ({
-  className: "row",
+  className: 'row',
 }))`
   height: ${(props) => {
-    if (props.half) return "48%";
-    else if (props.full) return "100%";
-    else if (props.auto) return "10%";
-    else if (props.dynamic) return "auto";
-    else return "30%";
+    if (props.half) return '48%';
+    else if (props.full) return '100%';
+    else if (props.auto) return '10%';
+    else if (props.dynamic) return 'auto';
+    else return '30%';
   }};
 `;
 export const ContainerColumn = styled.div.attrs((props) => ({
   className: props.className,
 }))`
   background-color: none;
-  height: ${(props) => (props.height ? props.height : "30%")};
+  height: ${(props) => (props.height ? props.height : '30%')};
 `;
 
 export const Input = styled.input.attrs((props) => ({
   type: props.type,
   name: props.name,
-  className: "form-control",
+  className: 'form-control',
   value: props.value,
   required: props.required,
-  min: props.min ? props.min : "auto",
+  min: props.min ? props.min : 'auto',
   pattern: props.pattern ? props.pattern : null,
 
   placeholder: props.placeholder,
@@ -116,8 +114,8 @@ export const Imageview = styled.img.attrs((props) => ({
   src: props.src,
   alt: props.alternate,
 }))`
-  height: ${(props) => (props.height ? props.height : "50%")};
-  width: ${(props) => (props.width ? props.width : "50%")};
+  height: ${(props) => (props.height ? props.height : '50%')};
+  width: ${(props) => (props.width ? props.width : '50%')};
 `;
 
 export const ImageTag = styled.image.attrs((props) => ({
@@ -127,8 +125,8 @@ export const ImageTag = styled.image.attrs((props) => ({
   height: 20%;
 `;
 export const Submitbutton = styled.button.attrs((props) => ({
-  type: props.type ? props.type : "button",
-  className: "form-control",
+  type: props.type ? props.type : 'button',
+  className: 'form-control',
 }))`
   background-color: ${ColorOne};
   color: white;
@@ -170,17 +168,17 @@ export const MarginAround = styled.div`
   margin: 2%;
 `;
 export const ToggleButton = styled.button.attrs((props) => ({
-  type: props.type ? props.type : "button",
-  className: "btn mt-3",
+  type: props.type ? props.type : 'button',
+  className: 'btn mt-3',
   value: props.value,
 }))`
-  background-color: ${(props) => (props.active ? ColorOne : "white")};
-  color: ${(props) => (!props.active ? ColorOne : "white")};
+  background-color: ${(props) => (props.active ? ColorOne : 'white')};
+  color: ${(props) => (!props.active ? ColorOne : 'white')};
   border: 1px solid ${ColorOne};
-  width: 80%;
+  width: auto;
+  min-width: 80%;
   &:hover {
     color: white;
     background-color: ${ColorOne};
-    width: 85%;
   }
 `;
